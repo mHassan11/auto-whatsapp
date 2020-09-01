4edbf67d63e8f178cea7a757814ccc44bb6d3cb5
mHassan11 <mhassan.3939@gmail.com>
2020-09-01 05:59:07 +0500

    update

--- test.py
+++ test.py
@@ -4,44 +4,51 @@ from selenium.webdriver.chrome.options import Options
 
 import time
 
-# options = webdriver.ChromeOptions();
-
 options = Options()
 options.add_argument('--user-data-dir=./User_Data')
 driver = webdriver.Chrome(options=options)
 driver.get('https://web.whatsapp.com/')
 
-# name = input('Enter the name of user or group : ')
-name = 'hassan'
+def send_message(name, msg):
+	flag = True
+
+	try:
+		user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+		user.click()
+
+		print("Writing msg")
+		wait = WebDriverWait(driver = driver, timeout = 90)
+		time.sleep(5)
+		msg_box = driver.find_element_by_class_name('_3uMse')
+		msg_box.send_keys(msg)
+
+		print("Pressing send button")
+		time.sleep(5)
+		wait = WebDriverWait(driver = driver, timeout = 90)
+		button = driver.find_element_by_class_name('_1U1xa')
+		button.click()
+
+	except:
+		flag = False
+		return False
+		
+	return flag
+
+#####
 name_list = ['hassan', 'Group1']
-# msg = input('Enter your message : ')
 msg = "hello1"
-# count = int(input('Enter the count : '))
-count = 2
-wait = WebDriverWait(driver = driver, timeout = 900)
+
+swait = WebDriverWait(driver = driver, timeout = 900)
 time.sleep(30)
 
-user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-user.click()
-
-# msg_box = driver.find_element_by_class_name('_2S1VP')
-# _2FbwG
-
-# msg += '\n this is a system generated message'
-
-for i in range(count):
-	print("count->", i+1)
-	wait = WebDriverWait(driver = driver, timeout = 90)
-	time.sleep(10)
-	msg_box = driver.find_element_by_class_name('_3uMse')
-	msg_box.send_keys(msg)
-
-	time.sleep(10)
-	wait = WebDriverWait(driver = driver, timeout = 90)
-	button = driver.find_element_by_class_name('_1U1xa')
-	button.click()
-
-wait = WebDriverWait(driver = driver, timeout = 600)
+for name in name_list:
+	print(name)
+	resp = send_message(name, msg)
+	if(resp == False):
+		print("Not done->", name,". Will try again")
+		name = name_list.append(name)
+	else:
+		print("Done->", name)
 
 time.sleep(120)
 driver.close()
